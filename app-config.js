c68e538c72442073ea99526f7c0532291ee13a1d
tuan0974316576-glitch <tuan0974316576-glitch@users.noreply.github.com>
2026-04-02 13:53:43 +0800

    Use canonical Firebase Functions speaking URL

--- app-config.js
+++ app-config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = window.APP_CONFIG || {
     // Firebase Functions speaking backend.
-    SPEAKING_API_BASE: 'https://speakingapi-m3nllrk67a-df.a.run.app'
+    SPEAKING_API_BASE: 'https://asia-east2-battleship-game-c0909.cloudfunctions.net/speakingApi'
 };
