9c5c8e73b9e84ba2504b2e00b0ee2834dac5bbdb
tuan0974316576-glitch <tuan0974316576-glitch@users.noreply.github.com>
2026-04-24 20:16:25 +0800

    Fix rank query rules and simplify service worker

--- sw.js
+++ sw.js
@@ -13,29 +13,3 @@ self.addEventListener('message', (event) => {
     self.skipWaiting();
   }
 });
-
-self.addEventListener('fetch', (event) => {
-  const request = event.request;
-  const url = new URL(request.url);
-  const isSameOrigin = url.origin === self.location.origin;
-  const isCriticalAsset =
-    request.method === 'GET' &&
-    isSameOrigin &&
-    (
-      request.mode === 'navigate' ||
-      request.destination === 'script' ||
-      request.destination === 'style' ||
-      request.destination === 'worker' ||
-      request.destination === 'manifest' ||
-      /\.(html|js|css|json)$/i.test(url.pathname)
-    );
-
-  if (!isCriticalAsset) {
-    event.respondWith(fetch(request));
-    return;
-  }
-
-  event.respondWith(
-    fetch(new Request(request, { cache: 'no-store' })).catch(() => fetch(request))
-  );
-});
