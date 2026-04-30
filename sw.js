a688a46cd23592a303f5f49c95b38586c130eaeb
tuan0974316576-glitch <tuan0974316576-glitch@users.noreply.github.com>
2026-04-30 13:07:32 +0800

    Bust PWA cache for app assets

--- sw.js
+++ sw.js
@@ -1,11 +1,17 @@
+self.APP_CACHE_VERSION = 'v0.44-20260430';
+
 self.addEventListener('install', (event) => {
-  console.log('[Service Worker] Install');
+  console.log('[Service Worker] Install', self.APP_CACHE_VERSION);
   event.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('[Service Worker] Activate');
-  event.waitUntil(self.clients.claim());
+  console.log('[Service Worker] Activate', self.APP_CACHE_VERSION);
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('message', (event) => {
