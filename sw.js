3b25c5ac86b66a5127535364047ffd54b3df9d66
tuan0974316576-glitch <tuan0974316576-glitch@users.noreply.github.com>
2026-05-08 22:52:31 +0800

    Align HUD rank label

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-self.APP_CACHE_VERSION = 'v0.56-20260508';
+self.APP_CACHE_VERSION = 'v0.57-20260508';
 
 self.addEventListener('install', (event) => {
   console.log('[Service Worker] Install', self.APP_CACHE_VERSION);
