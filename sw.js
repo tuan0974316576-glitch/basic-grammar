6f26f6bc535cc5e68f1eac1d5ab2ae6bd86c6f1a
tuan0974316576-glitch <tuan0974316576-glitch@users.noreply.github.com>
2026-05-13 15:38:29 +0800

    Bump version to 0.88l

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-self.APP_CACHE_VERSION = 'v0.88k-20260513';
+self.APP_CACHE_VERSION = 'v0.88l-20260513';
 
 self.addEventListener('install', (event) => {
   console.log('[Service Worker] Install', self.APP_CACHE_VERSION);
