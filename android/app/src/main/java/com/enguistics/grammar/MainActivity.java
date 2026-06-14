a3ff7ea0a065b8cd76867d24c8b8bb79bd9ec78f
MacBook <macbook@MacBookdeMacBook-Air.local>
2026-06-15 00:11:43 +0800

    Add Capacitor Android app shell

---
+++ android/app/src/main/java/com/enguistics/grammar/MainActivity.java
@@ -0,0 +1,5 @@
+package com.enguistics.grammar;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ scripts/build-web.js
@@ -0,0 +1,48 @@
+const fs = require("fs");
+const path = require("path");
+
+const rootDir = path.resolve(__dirname, "..");
+const outDir = path.join(rootDir, "www");
+
+const files = [
+  ".htaccess",
+  "app.js",
+  "grammar_core.js",
+  "grammar_data.js",
+  "grammar_verb_table_audio_manifest.js",
+  "grammar_verb_table_data.js",
+  "grammar_verb_table_image_manifest.js",
+  "index.html",
+  "style.css"
+];
+
+const directories = [
+  "assets",
+  "audio"
+];
+
+function copyFile(source, target) {
+  fs.mkdirSync(path.dirname(target), { recursive: true });
+  fs.copyFileSync(source, target);
+}
+
+function copyDirectory(source, target) {
+  fs.cpSync(source, target, {
+    recursive: true,
+    force: true,
+    filter: (itemPath) => !path.basename(itemPath).startsWith(".")
+  });
+}
+
+fs.rmSync(outDir, { recursive: true, force: true });
+fs.mkdirSync(outDir, { recursive: true });
+
+files.forEach((file) => {
+  copyFile(path.join(rootDir, file), path.join(outDir, file));
+});
+
+directories.forEach((directory) => {
+  copyDirectory(path.join(rootDir, directory), path.join(outDir, directory));
+});
+
+console.log(`Built web assets in ${outDir}`);
