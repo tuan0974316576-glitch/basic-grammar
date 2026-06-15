b6843f5eaa4948c6170f7944b87c96db63251632
MacBook <macbook@MacBookdeMacBook-Air.local>
2026-06-15 18:55:11 +0800

    Fill Firebase web config

--- firebase_config.js
+++ firebase_config.js
@@ -1,2 +1,9 @@
-window.GRAMMAR_FIREBASE_CONFIG = window.GRAMMAR_FIREBASE_CONFIG || null;
-
+window.GRAMMAR_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCfo5jxY1zvkRJPuPtZOMYj1V0kT7Te11A",
+  authDomain: "battleship-game-c0909.firebaseapp.com",
+  projectId: "battleship-game-c0909",
+  storageBucket: "battleship-game-c0909.firebasestorage.app",
+  messagingSenderId: "199612407738",
+  appId: "1:199612407738:web:3ed7840ec14992be7f8783",
+  functionsRegion: "asia-east2"
+};
